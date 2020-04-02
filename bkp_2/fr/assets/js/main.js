0c27b277dff093ab5bb05fa8d602c86a54e4350f
Riazul Islam <ri64627@gmail.com>
2020-04-02 15:17:27 +0600

    adde the xare project

---
+++ bkp_2/fr/assets/js/main.js
@@ -0,0 +1,11 @@
+(function ($) {
+  "use strict";
+
+  jQuery(document).ready(function ($) {
+    $('.case-study-area').owlCarousel({
+      items: 1,
+      loop: true,
+      margin: 0
+    });
+  });
+})(jQuery);
---
+++ rev-fixed-header-footer/fr/assets/js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+  "use strict";
+
+  jQuery(document).ready(function ($) {
+    $('.case-study-area').owlCarousel({
+      items: 1,
+      loop: true,
+      margin: 0,
+      autoHeight: true
+    });
+  });
+})(jQuery);
